d205f0c3198cef646925e7d987c3214db9b143a7
yoobilee <yblrrto23@gmail.com>
2023-08-02 14:11:50 +0900

    설명

--- MA/settings.py
+++ MA/settings.py
@@ -23,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#klki^5ne08-#%=01h2=2!us1w7f@re+6(n_+l&hu0-m7lh7u9'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
